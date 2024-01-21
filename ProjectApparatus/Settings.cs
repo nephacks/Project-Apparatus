9cd1051fb197394c136247849d1bb534148f6656
nephacks <37985788+nephacks@users.noreply.github.com>
2024-01-21 13:56:28 +0100

    ver

--- ProjectApparatus/Settings.cs
+++ ProjectApparatus/Settings.cs
@@ -10,7 +10,7 @@ namespace ProjectApparatus
     [System.Serializable]
     public class SettingsData
     {
-        public readonly string version = "1.03";
+        public readonly string version = "1.04";
 
         /* ESP */
         public bool b_EnableESP;
